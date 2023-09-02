90752ecc587d13bf3649abd3f0d5ee8272b779aa
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2023-09-02 17:41:57 +0100

    added uncaught exception handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
-import { Application } from 'express'
+import { Application, Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
 import 'dotenv/config'
 import './config/passport'
 import routes from './routes'
@@ -8,5 +9,10 @@ const app: Application = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err)
+    return res.status(500).json({ error: 'Internal server error, try again later' })
+})
 
 app.listen(process.env.PORT)
