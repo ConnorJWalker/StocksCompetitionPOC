d034ff34de3113d70dd4f2cc32302368235a7ef9
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2023-09-28 18:52:42 +0100

    fixed sell orders not being added to order histories

--- server/src/data-fetcher.ts
+++ server/src/data-fetcher.ts
@@ -65,6 +65,13 @@ const updateOpenPositions = async (users: IUserWithSecrets[]): Promise<void> => 
 
             if (t212Position === undefined) {
                 removedPositions.push(position)
+                newOrderHistory.push({
+                    userId: dbOpenPosition.user.id,
+                    type: 'sell',
+                    averagePrice: 0, // how to get???
+                    quantity: position.quantity,
+                    trading212Ticker: position.trading212Ticker
+                })
             }
             // If returned quantity is different to stored quantity by more than the database will truncate, add to database
             else if (t212Position.quantity !== position.quantity && Math.abs(t212Position.quantity - position.quantity) > 0.000009) {
