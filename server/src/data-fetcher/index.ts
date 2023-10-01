17135c34df610bde641839f3c918a148f04b8912
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2023-10-02 00:14:22 +0100

    added warning discord message on 401 t212 response

--- server/src/data-fetcher/index.ts
+++ server/src/data-fetcher/index.ts
@@ -4,6 +4,7 @@ import DatabaseService from '../shared/services/database-service'
 import { IUserWithSecrets } from '../shared/models/iuser'
 import OpenPositionsUpdater from './updaters/open-positions-updater'
 import AccountValueUpdater from './updaters/account-value-updater'
+import DiscordService from '../shared/services/discord-service'
 
 // store users and update on users update redis event
 let users: IUserWithSecrets[] = []
@@ -20,13 +21,28 @@ const accountValuesUpdater = new AccountValueUpdater(50, 25)
 setInterval(async () => {
     if (users.length === 0 || usersRequiresUpdate) {
         users = await DatabaseService.GetAllUsersWithValidApiKeys()
+        usersRequiresUpdate = false
     }
 
     await Promise.all([ accountValuesUpdater.update(users), openPositionsUpdater.update(users) ])
 
-    const unauthorisedUsers = Array.from(new Set([ ...accountValuesUpdater.unauthorizedUsers, ...openPositionsUpdater.unauthorizedUsers ]))
-    if (unauthorisedUsers.length !== 0) {
-        await DatabaseService.InvalidateApiKeys(unauthorisedUsers)
+    const unauthorisedUsersSet = new Set([ ...accountValuesUpdater.unauthorizedUsers, ...openPositionsUpdater.unauthorizedUsers ])
+    const unauthorisedUsersIds = Array.from(unauthorisedUsersSet)
+
+    if (unauthorisedUsersIds.length !== 0) {
+        await DatabaseService.InvalidateApiKeys(unauthorisedUsersIds)
+
+        const unauthorisedUsers = users.filter(user => unauthorisedUsersSet.has(user.id))
+            .map(user => user.discordUsername)
+
+        try {
+            await DiscordService.SendUnauthorisedWarnings(unauthorisedUsers)
+        }
+        catch (e) {
+            console.error(e)
+            // TODO: handle not sending properly
+        }
+
         usersRequiresUpdate = true
     }
 }, 6000)
--- server/src/shared/services/discord-service.ts
+++ server/src/shared/services/discord-service.ts
@@ -12,6 +12,14 @@ export class DiscordError extends Error {
     }
 }
 
+/**
+ * Searches redis cache for discord user object then requests discords api for the user object if not
+ * currently stored in redis. Refreshed cache with each request to discord
+ *
+ * @param {string} discordUsername Username to search discord server for
+ * @returns {Promise<IDiscordUser | null>} IDiscordUser if user was found in server, null if not found
+ * @throws {DiscordError} Thrown when user was not found in cache and discord return non 200 response
+ */
 const fetchUser = async (discordUsername: string): Promise<IDiscordUser | null> => {
     const cachedUser = await Redis.hGetAll(`discord:${discordUsername}`)
 
@@ -41,6 +49,14 @@ const fetchUser = async (discordUsername: string): Promise<IDiscordUser | null> 
     return user
 }
 
+/**
+ * Searches the discord server for a user with a matching discord username
+ *
+ * @param {string} discordUsername Username of user to search for
+ * @returns {Promise<boolean>} True if user was found in discord server, false if not
+ * @throws {DiscordError} Thrown when provided username is same as bots username
+ * @async
+ */
 const UserIsInDiscord = async (discordUsername: string): Promise<boolean> => {
     if (discordUsername === process.env.DISCORD_BOT_NAME) {
         throw new DiscordError('you are not the discord bot', 400)
@@ -49,6 +65,15 @@ const UserIsInDiscord = async (discordUsername: string): Promise<boolean> => {
     return await fetchUser(discordUsername) !== null
 }
 
+/**
+ * Converts user object returned from discords api into profile picture url
+ *
+ * @see {@link https://discord.com/developers/docs/reference#image-formatting}
+ * @param {string} discordUsername Username of user to get profile picture of
+ * @returns {Promise<string>} Url to users profile picture
+ * @throws {DiscordError} Thrown when the user could not be found in the server
+ * @async
+ */
 const GetProfilePicture = async (discordUsername: string): Promise<string> => {
     const user = await fetchUser(discordUsername)
     if (user === null) {
@@ -61,6 +86,13 @@ const GetProfilePicture = async (discordUsername: string): Promise<string> => {
         : `${baseUrl}avatars/${user.id}/${user.avatar}.png`
 }
 
+/**
+ * Sends message welcoming user to the competition
+ *
+ * @param {string} discordUsername Username of user to welcome
+ * @param {boolean} isCompetitor True if user is competing and provided api key
+ * @async
+ */
 const SendWelcomeMessage = async (discordUsername: string, isCompetitor: boolean) => {
     const message = isCompetitor ? 'a competitor' : 'an observer'
     await send(`channels/${process.env.DISCORD_CHANNEL_ID}/messages`, 'post', {
@@ -69,6 +101,35 @@ const SendWelcomeMessage = async (discordUsername: string, isCompetitor: boolean
     })
 }
 
+/**
+ * Sends warning message for users who got an unauthorised response from trading212
+ *
+ * @param {string[]} discordUsernames Array of discord usernames to warn
+ * @async
+ */
+const SendUnauthorisedWarnings = async (discordUsernames: string[]) => {{
+    const usersString = discordUsernames.join(', ')
+    const plural = discordUsernames.length > 1
+
+    const message = `${usersString} your apiKey${plural ? 's' : ''} are no longer authorised `
+        + `please provide a new api key, your disqualification countdown${plural ? 's have' : ' has'} started`
+
+    await send(`channels/${process.env.DISCORD_CHANNEL_ID}/messages`, 'post', {
+        content: message,
+        tts: false
+    })
+}}
+
+/**
+ * Sends request to the discord api with required headers, parsing the received response
+ *
+ * @template T
+ * @param {string} endpoint Discord api endpoint to send request to
+ * @param {string} method Http method to use
+ * @param {object | undefined} body Http request body. Only applies when method is not get
+ * @returns {Promise<IHttpResult<T | null>>} Promise containing parsed response body or null on non 200 response
+ * @async
+ */
 const send = async <T>(endpoint: string, method: string = 'get', body: object | undefined = undefined): Promise<IHttpResult<T | null>> => {
     const response = await fetch(`${process.env.DISCORD_URL}${endpoint}`, {
         method,
@@ -89,5 +150,6 @@ const send = async <T>(endpoint: string, method: string = 'get', body: object | 
 export default {
     UserIsInDiscord,
     GetProfilePicture,
-    SendWelcomeMessage
+    SendWelcomeMessage,
+    SendUnauthorisedWarnings
 }
