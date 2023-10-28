a5172fdce6ffe094f89ce7ccbea66f921afa6128
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2023-10-28 12:50:27 +0100

    fixed excessive rate limits

--- applications/api/middleware/rate-limit.ts
+++ applications/api/middleware/rate-limit.ts
@@ -1,15 +1,18 @@
 import { DraftHeadersVersion, rateLimit } from 'express-rate-limit'
+import { Request, Response } from 'express'
 
 const options = {
     standardHeaders: 'draft-7' as DraftHeadersVersion,
     legacyHeaders: false,
     statusCode: 429,
-    message: () => ({ error: 'too many requests' })
+    message: () => ({ error: 'too many requests' }),
+    keyGenerator: (req: Request, res: Response) => (req.headers['x-forwarded-for'] || req.ip) as string
 }
 
 export const authenticationLimiter = rateLimit({
     windowMs: 10 * 60 * 1000,
     limit: 3,
+    skipSuccessfulRequests: true,
     ...options
 })
 
